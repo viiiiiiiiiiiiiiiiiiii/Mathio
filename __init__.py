223690c0ea21dfa7e0e78e3c2855c5fa8bc838ed
Vi <kunykunykuny6@gmail.com>
2024-09-22 11:27:02 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from .basic_operations import add, subtract, multiply, divide
+from .trigonometry import sin, cos, tan
+from .roots import square_root, cube_root
+from .logarithms import natural_log
+from .calculus import derivative, definite_integral, indefinite_integral
+from .algebra import solve_linear_equations, quadratic_formula
+from .statistics import mean, median, mode, standard_deviation
+from .geometry import area_of_circle, perimeter_of_circle, area_of_triangle, area_of_rectangle
