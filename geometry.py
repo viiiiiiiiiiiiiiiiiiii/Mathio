1b4aededa155071cc88d1a87819824b898e3e550
Vi <kunykunykuny6@gmail.com>
2024-09-22 11:26:44 +0300

    Create geometry.py

---
+++ geometry.py
@@ -0,0 +1,11 @@
+def area_of_circle(radius):
+    return 3.14159 * radius ** 2
+
+def perimeter_of_circle(radius):
+    return 2 * 3.14159 * radius
+
+def area_of_triangle(base, height):
+    return 0.5 * base * height
+
+def area_of_rectangle(length, width):
+    return length * width
